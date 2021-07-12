8fa572b2c27b39a3d2dba549932e741a4c030d84
dim50dim <zheliezkov@gmail.com>
2021-07-12 09:39:38 +0300

    first task

--- js/script.js
+++ js/script.js
@@ -1,16 +1,13 @@
 /////////111111111111111111
 
-function main (p) {
-     p = 0;
-    return function one () {
-       return p+= 3;
-    };
+function main () {
+   let p = 0;
+    return function (z) {
+      p += z;
+      return p;    
+    } 
 }
 let next = main();
-console.log(next());
-console.log(next());
-console.log(next());
-console.log(next());
-console.log(next());
-console.log(next());
-console.log(next());
+console.log(next(2));
+console.log(next(35));
+console.log(next(44));
