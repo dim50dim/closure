943a4766bf1376ccfefd1c4997c803953a4c61e3
dim50dim <zheliezkov@gmail.com>
2021-07-13 06:55:54 +0300

    second task

--- js/script.js
+++ js/script.js
@@ -1,13 +1,24 @@
 /////////111111111111111111
 
-function main () {
-   let p = 0;
+function  main(p) {
+     p = 0;
     return function (z) {
       p += z;
       return p;    
-    } 
+    }; 
 }
 let next = main();
-console.log(next(2));
-console.log(next(35));
-console.log(next(44));
+console.log(next(33));
+console.log(next(33));
+console.log(next(33));
+console.log(next(33));
+//////////////////2222222222222222
+
+function second(p){
+    return (z) => {
+      return   p + z;   
+    } 
+}
+let plane = second(100);
+console.log(plane(20));
+console.log(plane(25));
